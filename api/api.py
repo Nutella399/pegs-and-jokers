c9cc0ae866c01a139df1fe7a6edb0b818fa18c93
nutella399 <nutella399@gmail.com>
2024-12-17 16:17:23 -0800

    hard code hand for front end development

--- api/api.py
+++ api/api.py
@@ -1,8 +1,21 @@
 import time
+from datetime import datetime
+
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/time')
 def get_current_time(): 
-    return {'time': time.localtime()}
+    time_arr = time.localtime()
+    parsed_time = "{}-{}-{} {}:{}:{}".format(time_arr[0], time_arr[1], 
+                                             time_arr[2], time_arr[3], 
+                                             time_arr[4], time_arr[5])
+    return {'time': parsed_time}
+
+@app.route('/hand/<int:user_id>', methods=['GET'])
+def getHand(user_id): 
+    if(user_id == 1): 
+        return {'hand': ['Joker', 'King', '8', '7', 'Ace']}
+    else: 
+        return {'hand': ['9', '1', '2', '3', '4']}
--- src/Game.jsx
+++ src/Game.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import GameRules from "./GameRules";
-import "./Game.css"
+import "./Game.css";
 
 const Game = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -10,10 +10,14 @@ const Game = () => {
   };
   return (
     <div>
-      <h1>Game Board Coming Soon TM </h1>
-      <button class="guide-button"onClick={openRules}>Game Guide</button>
+      <header class="Game-header">
+        <h1>Game Board Coming Soon TM </h1>
+        <button class="guide-button" onClick={openRules}>
+          Game Guide
+        </button>
 
-      {isOpen && <GameRules closeGuide={() => setIsOpen(false)} />}
+        {isOpen && <GameRules closeGuide={() => setIsOpen(false)} />}
+      </header>
     </div>
   );
 };
